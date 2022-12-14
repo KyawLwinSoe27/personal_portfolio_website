6fc9245b491e3c4c181dcb52755dcd40e724d997
KyawLwinSoe27 <soek38902@gmail.com>
2022-12-14 21:09:50 +0630

    service section page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import "./index.css";
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero';
+import Services from './components/Services/Services';
 
 const App = () => {
   return (
    <div>
     <Navbar />
     <Hero />
+    <Services />
    </div>
   )
 }
